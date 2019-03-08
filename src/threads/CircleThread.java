9ddb85d20615c22874fd4a600fbeb36e3787f807
Daniel Fernandez <alejo.dafr@gmail.com>
2019-03-07 23:10:11 -0500

    first commit

--- src/threads/CircleThread.java
+++ src/threads/CircleThread.java
@@ -1,22 +1,43 @@
 package threads;
 
+import javafx.scene.shape.Circle;
 import ui.CircleController;
 
 public class CircleThread extends Thread{
 	private CircleController circleController;
-	
-	public CircleThread(CircleController circleC) {
+	private Circle circle;
+	public boolean endOfApplication = false;
+	public static boolean moveOn = false;
+	private int bounces;
+
+	public CircleThread(CircleController circleC, Circle circle) {
 		circleController = circleC;
+		this.circle = circle;
+		bounces = 0;
 	}
-	
+
 	public void run() {
 		while(true) {
-			circleController.moveBlueCircle();
-			try {
-				sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			if(moveOn) {
+				System.out.print("");
+				if(!circleController.moveCircle(circle, bounces)) {
+					bounces += 1;
+					if(circle.getLayoutX() < 0) {
+						circle.setLayoutX(5);
+					}
+					else {
+						circle.setLayoutX(550);
+					}
+				}
+				try {
+					sleep(100);
+				} catch (InterruptedException e) {
+				}
 			}
 		}
+	}
+	
+	public void setMoveOn(boolean move) {
+		moveOn = move;
 	}
 }
--- src/ui/CircleController.java
+++ src/ui/CircleController.java
@@ -1,23 +1,70 @@
 package ui;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import threads.CircleThread;
 
 public class CircleController {
 
-    @FXML
-    private Circle blueCircle;
+	@FXML
+	private Circle blueCircle;
 
-    @FXML
-    public void moveCircle(ActionEvent event) {
-    	CircleThread ct = new CircleThread(this);
-    	ct.start();
-    }
-    
-    public void moveBlueCircle() {
-    	blueCircle.setLayoutX(blueCircle.getLayoutX()+5);
-    }
+	@FXML
+	private Pane map;
+	
+	private ExecutorService ex;
+	
+	private CircleThread ct;
 
+	@FXML
+	public void initialize() {
+		ct = new CircleThread(this, blueCircle);
+		blueCircle.setLayoutY(4000);
+		ex = Executors.newCachedThreadPool();
+		ex.execute(ct);
+	}
+
+	@FXML
+	public void moveCircle(ActionEvent event) {
+		if(!CircleThread.moveOn) {
+			ct.setMoveOn(true);
+		}
+	}
+
+	public boolean moveCircle(Circle circle, int bounces) {
+		if(circle.getLayoutX() >= 0 && circle.getLayoutX()+circle.getRadius() < 580) {
+			if(bounces%2 == 0) {
+				circle.setLayoutX(circle.getLayoutX()+5);
+			}
+			else {
+				circle.setLayoutX(circle.getLayoutX()-5);
+			}
+		}
+		return circle.getLayoutX() >= 0 &&
+				circle.getLayoutX() + circle.getRadius() < 580;
+	}
+
+	@FXML
+	public void createCircle(MouseEvent event) {
+		Circle circle = new Circle(17, blueCircle.getFill());
+		circle.setStroke(blueCircle.getStroke());
+		circle.setLayoutX(event.getX());
+		circle.setLayoutY(event.getY());
+		CircleThread cirt = new CircleThread(this, circle);
+		ex.execute(cirt);
+		map.getChildren().add(circle);
+	}
+	
+	@FXML
+	public void stopCircles(ActionEvent event) {
+		if(CircleThread.moveOn) {
+			ct.setMoveOn(false);
+		}
+	}
 }
